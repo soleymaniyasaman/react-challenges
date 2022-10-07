215676cbe564bee37fa192f095acdd67edbfe73e
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2022-10-07 13:17:19 +0300

    react challenges color render , dark mode and form validator

---
+++ src/01-color-renderer/ColorRenderer.js
@@ -0,0 +1,26 @@
+import Color from './Color'
+
+const colors = [{
+  hex: '#91A6FF',
+  name: 'Cornflower Blue'
+},
+{
+  hex: '#FF88DC',
+  name: 'Persian Pink'
+},
+{
+  hex: '#80FF72',
+  name: 'Screamin Green'
+},
+{
+  hex: '#FF5154',
+  name: 'Tart Orange'
+}]
+
+export default function ColorRenderer() {
+  return (
+    <h2>
+      {colors.map(color => <Color key={color.hex} hex={color.hex} name={color.name} />)}
+    </h2>
+  )
+}
