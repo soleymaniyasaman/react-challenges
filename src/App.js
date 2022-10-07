7712ea5af7ee68d989d77ef7ac3feca0fd6b50ef
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2022-10-07 15:43:01 +0300

    keep score in localStorage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import ColorRenderer from './01-color-renderer/ColorRenderer'
 import DarkMode from './02-dark-mode/DarkMode'
 import FormValidator from './03-form-validator/FormValidator'
 import DogPics from './04-dog-pic/DogPics'
+import ScoreKeeper from './05-score-keeper/ScoreKeeper'
 
 function App() {
   return (
     <div className='App'>
-      <DogPics />
+      <ScoreKeeper />
     </div>
   )
 }
