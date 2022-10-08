00faa91d3b35e1a781f50b2cb59c72ee58ebacdf
YasaminSoleimani <soleymani.yasaman@yahoo.com>
2022-10-08 20:19:12 +0300

    shopping cart with increase and decreas button

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ import ColorPicker from './07-color-picker/ColorPicker'
 import PixelArt from './08-pixel-art/PixelArt'
 import SimpleCalculator from './09-simple-calc/SimpleCalc'
 import FocusInput from './10-focus-input/FocusInput'
+import ShoppingCart from './11-shopping-cart/ShoppingCart'
 
 function App() {
   return (
     <div className='App'>
-      <FocusInput />
+      <ShoppingCart />
     </div>
   )
 }
